abc67344e219a3d3ac2996a9a81ed1e9e8159900
Dominik Rask <dominik.rask@gmail.com>
2011-02-16 22:24:53 +0100

    ntpl test added, make sure to run 'npm install nTPL'

--- bench.js
+++ bench.js
@@ -1,8 +1,11 @@
 var bm = require('./common'),
     ejs = require('./ejs/lib/ejs'),
+    ntpl = require('./ntpl/lib/nTPL/nTPL'),
     fs = require('fs'),
     sys = require('sys');
 
+
+// #### ejs test
 var str = fs.readFileSync(__dirname + '/bigtable.ejs', 'ascii');
 
 var n = bm.times;
@@ -20,4 +23,30 @@ bm.start('ejs execution');
 while (n--) {
     ejs.render(str, { locals: bm.contexts.bigtable, cache: true, filename: 'example.ejs' });
 }
+bm.stop();
+
+
+// #### ntpl test
+var str = fs.readFileSync(__dirname + '/bigtable.ntpl', 'ascii');
+
+var n = bm.times;
+//sys.print(ejs.render(str, { locals: bm.contexts.bigtable }));
+
+var result = ntpl.nTPL({
+	template: str,
+	args: ["table"]
+})({table: bm.contexts.bigtable.table});
+var templatefunc = ntpl.nTPL({
+	template: str,
+	args: ["table"]
+});
+
+//sys.puts(result);
+
+bm.start('nTPL compilation');
+while (n--) {
+    templatefunc({table: bm.contexts.bigtable.table});
+}
+var duration = bm.stop();
+sys.print('Time per template: ' + duration / bm.times + ' ms\n');
 bm.stop();
